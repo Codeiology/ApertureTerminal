c9a7771003a134df68f107ab2bdf5ac99f49cb36
Codeiology <131825227+Codeiology@users.noreply.github.com>
2023-05-19 10:55:30 -0700

    Create customcommands.sh

---
+++ customcommands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function glados() {
+ python3 ~/Desktop/ApertureTerminal/GLaDOS.py
+}
+
+function nyan() {
+ curl https://ascii.live/nyan
+}
+
+function rickroll() {
+ curl https://ascii.live/rick
+}
+
+function help() {
+ cat ~/Desktop/ApertureTerminal/help.txt
+}
